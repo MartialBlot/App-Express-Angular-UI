b42d72659458fb9331758f0d00f6963349dcbba9
BLOT Martial <martial.blot@outlook.fr>
2019-07-12 15:26:39 +0200

    style differents component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VoirPlaylistComponent } from './voir-playlist/voir-playlist.component';
+import { CreerPlaylistComponent } from './creer-playlist/creer-playlist.component';
+import { CreerMorceauComponent } from './creer-morceau/creer-morceau.component';
 
-const routes: Routes = [ {path: "", component: HomeComponent}, ];
+const routes: Routes = [ {path: "", component: HomeComponent},
+{path: "playlists", component: VoirPlaylistComponent},
+{path: "newplaylist", component: CreerPlaylistComponent},
+{path: "newtrack", component: CreerMorceauComponent} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
